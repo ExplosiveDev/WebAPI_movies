391f5e8024e2094b7c9d0833f66a83826fb13201
SystemX <SystemX@DESKTOP-4R36CSF>
2023-09-14 21:10:54 +0300

    Add project files.

---
+++ DataAccess/Data/CinemaDBContext.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.EntiitesConfiguration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.Data
+{
+    public class CinemaDBContext:DbContext
+    {
+        public CinemaDBContext(DbContextOptions<CinemaDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.SeedMovie();
+
+
+            #region Fluent API => Configurations
+            ////Set Primary Key
+            //modelBuilder.Entity<Product>().HasKey(x => x.Id);
+
+            ////Set Property configurations
+            //modelBuilder.Entity<Product>()
+            //            .Property(x => x.Name)
+            //            .HasMaxLength(150)
+            //            .IsRequired();
+
+            ////Set Relationship configurations
+            //modelBuilder.Entity<Product>()
+            //    .HasOne<Category>(x => x.Category)
+            //    .WithMany(x => x.Products)
+            //    .HasForeignKey(x => x.CategoryId);
+            #endregion
+
+            // ApplyConfigurations for Entities 
+            // modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShopMVCDbContext).Assembly);
+            // or 
+            modelBuilder.ApplyConfiguration(new MovieConfiguration());
+
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using System.Linq.Expressions;
+
+namespace DataAccess.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            params string [] includeProperties);
+        Task <TEntity> GetByID(object id);
+        Task Insert(TEntity entity);
+        Task Delete(object id);
+        Task Delete(TEntity entityToDelete);
+        Task Update(TEntity entityToUpdate);
+        Task Save();
+    }
+}
